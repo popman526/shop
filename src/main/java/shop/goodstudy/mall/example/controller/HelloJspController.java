5d6389c6d0026f1328b7a6c649b93f14e244e7ba
Sangho Joo <popman526@naver.com>
2018-10-28 00:02:35 +0900

    init

---
+++ src/main/java/shop/goodstudy/mall/example/controller/HelloJspController.java
@@ -0,0 +1,13 @@
+package shop.goodstudy.mall.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloJspController {
+	
+	@GetMapping("/hello")
+	public String helloJsp() {
+		return "example/hello";
+	}
+}
