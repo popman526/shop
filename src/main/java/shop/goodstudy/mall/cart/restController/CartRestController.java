d1383ef39c952a37dbc76e1e2ead69e73c66d81a
kchan <kchan@DESKTOP-7E4SQQD>
2019-01-31 17:50:22 +0900

    장바구니 테스트

--- src/main/java/shop/goodstudy/mall/cart/restController/CartRestController.java
+++ src/main/java/shop/goodstudy/mall/cart/restController/CartRestController.java
@@ -1,12 +1,18 @@
 package shop.goodstudy.mall.cart.restController;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import shop.goodstudy.mall.cart.model.Cart;
 import shop.goodstudy.mall.cart.service.CartService;
+import shop.goodstudy.mall.customer.model.Customer;
+import shop.goodstudy.mall.product.model.Product;
 
 @RestController
 public class CartRestController {
@@ -17,6 +23,12 @@ public class CartRestController {
 	@PostMapping("cart")
 	public boolean postCart(@ModelAttribute Cart c) {
 		return cartService.create(c);
+	}
+	
+	/** 카트 리스트 가져오기 **/
+	@GetMapping("cart")
+	public List<Product> getCart(@SessionAttribute Customer customer){
+		return cartService.getList(customer.getCustomer_id());
 	}
 	
 }
--- src/main/java/shop/goodstudy/mall/cart/service/CartService.java
+++ src/main/java/shop/goodstudy/mall/cart/service/CartService.java
@@ -1,12 +1,16 @@
 package shop.goodstudy.mall.cart.service;
 
+import java.util.List;
+
 import org.springframework.transaction.annotation.Transactional;
 
 import shop.goodstudy.mall.cart.model.Cart;
+import shop.goodstudy.mall.product.model.Product;
 
 @Transactional
 public interface CartService {
 
 	boolean create(Cart c);
+	List<Product> getList(String id);
 	
 }//end of CartService
